976fac7f0a3afa7f644bfbadca6d1c5b2ea7b85a
vikivero <vikichan.ch@gmail.com>
2024-04-13 11:10:37 +0200

    Creates base project for backend

---
+++ backend/src/main/java/com/beautysaloon/beauty_saloon/BeautySaloonApplication.java
@@ -0,0 +1,13 @@
+package com.beautysaloon.beauty_saloon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySaloonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySaloonApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/beautysaloon/beauty_saloon/BeautySaloonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautysaloon.beauty_saloon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySaloonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
