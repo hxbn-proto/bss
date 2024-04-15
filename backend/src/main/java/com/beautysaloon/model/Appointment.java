2583b0970ab8ec162e903e27e1ef2c07a9e82554
vikivero <vikichan.ch@gmail.com>
2024-04-15 17:21:04 +0200

    ads new fields to apointment clas

--- backend/src/main/java/com/beautysaloon/model/Appointment.java
+++ backend/src/main/java/com/beautysaloon/model/Appointment.java
@@ -2,7 +2,12 @@ package com.beautysaloon.model;
 
 import lombok.Data;
 
+import java.time.Instant;
+
 @Data
 public class Appointment {
     private Long id;
+    private String patientName;
+    private Instant time;
+    private BeautyMaster beautyMaster;
 }
