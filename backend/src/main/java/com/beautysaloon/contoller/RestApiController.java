298cd4fd3f94f58dd29d02f69b79c87e80e5a814
antonina-dominiak <antonina.dominiak01@gmail.com>
2024-04-15 19:18:30 +0200

    Refactors rest api controller method logic to return busy dates instead of free dates

--- backend/src/main/java/com/beautysaloon/contoller/RestApiController.java
+++ backend/src/main/java/com/beautysaloon/contoller/RestApiController.java
@@ -1,22 +1,38 @@
 package com.beautysaloon.contoller;
 
+import com.beautysaloon.model.Appointment;
+import com.beautysaloon.model.BeautyMaster;
 import com.beautysaloon.repository.BeautyMasterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
+
+    @Autowired
     private BeautyMasterRepository beautyMasterRepository;
 
-    @GetMapping("/available-dates")
-    public ResponseEntity<List<String>> getAllAvailableDatesForAllMasters() {
-        List<String> masters = null;
-        return new ResponseEntity<>(masters, HttpStatus.OK);
+    @GetMapping("/show-busy")
+    public ResponseEntity<Map<String, List<Instant>>> getAllBusyDatesForAllMasters() {
+        List<BeautyMaster> masters = beautyMasterRepository.findAll();
+
+        Map<String, List<Instant>> busyDates = masters.stream()
+                .collect(Collectors.toMap(
+                        BeautyMaster::getName,
+                        master -> master.getAppointments().stream()
+                                .map(Appointment::getTime)
+                                .collect(Collectors.toList())));
+
+        return new ResponseEntity<>(busyDates, HttpStatus.OK);
     }
 }
