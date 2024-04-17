50f16e868ccb185c7fd95313f7d3063b8edf0655
antonina-dominiak <antonina.dominiak01@gmail.com>
2024-04-17 18:05:56 +0200

    Adds basic validation

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 
 const router = express.Router();
 
@@ -6,9 +7,25 @@ router.get("/", (req, res) => {
   res.render("form", { title: "Registration form" });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.render("form", { title: "Registration form" });
-});
+router.post(
+  "/",
+  [
+    check("name").isLength({ min: 1 }).withMessage("Please enter a name"),
+    check("email").isLength({ min: 1 }).withMessage("Please enter an email"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      res.send("Thank you for your registration!");
+    } else {
+      res.render("form", {
+        title: "Registration form",
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
 
 module.exports = router;
