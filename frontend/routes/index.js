8ea50b714c8abbbe90a418b8771135381749c15b
vikivero <vikichan.ch@gmail.com>
2024-04-17 17:11:12 +0200

    Adds logic to parse basic form data

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -6,4 +6,9 @@ router.get("/", (req, res) => {
   res.render("form", { title: "Registration form" });
 });
 
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.render("form", { title: "Registration form" });
+});
+
 module.exports = router;
